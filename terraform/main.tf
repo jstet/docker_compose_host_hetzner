0e6ecdaa5b097d02700939cb73c46c625e321cb1
Jonas Stettner <mail@jstet.net>
2023-07-19 03:16:50 +0200

    fix cloud init

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ data "hetznerdns_zone" "dns_zone" {
 }
 
 resource "hcloud_firewall" "firewall" {
-  name = var.subdomain
+  name = "docker_host"
   rule {
     direction = "in"
     protocol  = "tcp"
@@ -57,9 +57,13 @@ package_update: true
 package_upgrade: true
 package_reboot_if_required: false
 manage_etc_hosts: true
-locale: en_US.UTF-8
-timezone: Europe/Berlin
 
+network:
+  version: 2
+  ethernets:
+    eth0:
+      nameservers:
+        addresses: [8.8.8.8, 8.8.4.4]
 
 packages:
   - apt-transport-https
@@ -74,14 +78,11 @@ runcmd:
   - install -m 0755 -d /etc/apt/keyrings
   - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   - chmod a+r /etc/apt/keyrings/docker.gpg
-  - echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-  - apt-get update -y
-  - apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin a
-  - sudo apt-add-repository ppa:ansible/ansible
-  - sudo apt install ansible
+  - echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  - apt-get update
+  - apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   - printf "[sshd]\nenabled = true\nbanaction = iptables-multiport" > /etc/fail2ban/jail.local
   - systemctl enable fail2ban
-  - apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   - sed -i -e '/^\(#\|\)PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config
   - sed -i -e '/^\(#\|\)PasswordAuthentication/s/^.*$/PasswordAuthentication no/' /etc/ssh/sshd_config
   - sed -i -e '/^\(#\|\)X11Forwarding/s/^.*$/X11Forwarding no/' /etc/ssh/sshd_config
@@ -89,13 +90,9 @@ runcmd:
   - sed -i -e '/^\(#\|\)AllowTcpForwarding/s/^.*$/AllowTcpForwarding no/' /etc/ssh/sshd_config
   - sed -i -e '/^\(#\|\)AllowAgentForwarding/s/^.*$/AllowAgentForwarding no/' /etc/ssh/sshd_config
   - sed -i -e '/^\(#\|\)AuthorizedKeysFile/s/^.*$/AuthorizedKeysFile .ssh\/authorized_keys/' /etc/ssh/sshd_config
-  - sed -i '$a AllowUsers holu' /etc/ssh/sshd_config
   - systemctl daemon-reload
   - systemctl restart docker
   - systemctl enable docker
-
-groups:
-  - docker
 
 users:
   - default
@@ -105,10 +102,9 @@ users:
     lock_passwd: true
     shell: /bin/bash
     ssh_authorized_keys:
-    - ${file(var.ssh_key_path)}
+      - ${file(var.ssh_key_path)}
 
 final_message: "The system is ready, after $UPTIME seconds"
-
 EOF
 }
 
